1a97c2ed26e142411a43cb3020c6c7541c6b680a
Hernán Chamorro <lc.hernanchamorro@gmail.com>
2020-12-04 01:43:52 -0300

    final

---
+++ grunt.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: "css",
+            src: ["*.scss"],
+            dest: "css",
+            ext: ".css",
+          },
+        ],
+      },
+    },
+  });
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.registerTasks("css", ["sass"]);
+};
